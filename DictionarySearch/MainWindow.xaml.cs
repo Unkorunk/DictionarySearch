0a41cc4d408603ade6a10251c0fec1ffd3002633
Vladislav Kalinin <unkorunk@gmail.com>
2021-04-08 18:00:36 +1000

    Initial project

---
+++ DictionarySearch/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DictionarySearch
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private readonly SearchWorker _searchWorker = new SearchWorker();
+        private BackgroundWorker _backgroundWorker = new BackgroundWorker();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            StartWorker();
+        }
+
+        private void DoWork(object sender, DoWorkEventArgs args) => Thread.Sleep(1000 / 60);
+
+        private void RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs args)
+        {
+            lock (_searchWorker.ResultLock)
+            {
+                if (_searchWorker.IsFresh) OutputListView.Items.Clear();
+                _searchWorker.IsFresh = false;
+                for (var i = 0; i < 1000 && _searchWorker.Result.Count != 0; i++)
+                {
+                    OutputListView.Items.Add(_searchWorker.Result.Dequeue());
+                }
+
+                Status.Content = OutputListView.Items.Count == 0
+                    ? "По вашему запросу ничего не нашлось"
+                    : $"Нашлось {OutputListView.Items.Count} результатов";
+            }
+
+            StartWorker();
+        }
+
+        private void StartWorker()
+        {
+            _backgroundWorker = new BackgroundWorker();
+            _backgroundWorker.DoWork += DoWork;
+            _backgroundWorker.RunWorkerCompleted += RunWorkerCompleted;
+            _backgroundWorker.RunWorkerAsync();
+        }
+
+        private void UpdateInterface()
+        {
+            _searchWorker.UpdateSearchString(InputTextBox.Text, IsSeqSearch.IsChecked == true);
+        }
+
+        private void InputTextBox_OnTextChanged(object sender, TextChangedEventArgs e) => UpdateInterface();
+        private void IsSeqSearch_OnEvent(object sender, RoutedEventArgs e) => UpdateInterface();
+    }
+}
---
+++ DictionarySearch/SearchWorker.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace DictionarySearch
+{
+    public class SearchWorker
+    {
+        private Thread _thread;
+        private StreamReader _streamReader;
+        private string _searchString = string.Empty;
+        private bool _isSeqSearch = true;
+        private readonly object _searchLock = new object();
+
+        public bool IsFinished { get; private set; }
+        public bool IsFresh { get; set; }
+        public object ResultLock { get; } = new object();
+        public Queue<string> Result { get; private set; } = new Queue<string>();
+
+        public SearchWorker()
+        {
+            _streamReader = new StreamReader("words.txt");
+            _thread = new Thread(Run) {IsBackground = true};
+            _thread.Start();
+        }
+
+        public void UpdateSearchString(string searchString, bool isSeqSearch)
+        {
+            lock (_searchLock)
+            {
+                IsFinished = false;
+
+                _streamReader.Close();
+                _streamReader = new StreamReader("words.txt");
+
+                _searchString = searchString;
+                _isSeqSearch = isSeqSearch;
+
+                lock (ResultLock)
+                {
+                    IsFresh = true;
+                    Result.Clear();
+                }
+            }
+        }
+
+        private void Run()
+        {
+            while (true)
+            {
+                lock (_searchLock)
+                {
+                    if (_streamReader.EndOfStream)
+                    {
+                        IsFinished = true;
+                        continue;
+                    }
+
+                    var target = _streamReader.ReadLine();
+                    if (!MyCompare(_searchString, target)) continue;
+
+                    lock (ResultLock)
+                    {
+                        Result.Enqueue(target);
+                    }
+                }
+            }
+        }
+
+        private bool MyCompare(string input, string target)
+        {
+            return _isSeqSearch ? MyCompareSeq(input, target) : MyCompareSubStr(input, target);
+        }
+
+        private static bool MyCompareSubStr(string input, string target) => target.Contains(input);
+
+        private static bool MyCompareSeq(string input, string target)
+        {
+            var i = 0;
+            foreach (var t in input)
+            {
+                while (i < target.Length && t != target[i]) i++;
+                if (i >= target.Length) return false;
+                i++;
+            }
+
+            return true;
+        }
+    }
+}
